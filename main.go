92ae2e573f9c2f4cd8b6042602d2364eed6992bd
Gabriel Killy <gabykilly@gmail.com>
2022-04-05 21:01:44 -0500

    correction to main

--- main.go
+++ main.go
@@ -2,17 +2,19 @@ package main
 
 import (
 	"gol/the-basics/dev/config"
+	"gol/the-basics/dev/db"
 	"gol/the-basics/dev/service"
-	"gol/the-basics/dev/util"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	util.InitializeSingletons(config.GlobalConfig)
+	globalConfig := config.NewGlobalConfig()
+	database := db.NewDatabase(&globalConfig)
+	userService := service.NewUserService(&database)
 
 	router := gin.Default()
 	router.Use(gin.Logger())
-	router.POST("/user", service.CreateUser)
+	router.POST("/user", userService.CreateUser)
 	router.Run("localhost:8080")
 }
